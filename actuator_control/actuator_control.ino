65b82f3c4aaadb464637776c7a04a9257836ccdf
dylannl2 <dylannl2@illinois.edu>
2022-11-20 17:33:54 -0500

    changing max height

--- actuator_control/actuator_control.ino
+++ actuator_control/actuator_control.ino
@@ -11,12 +11,13 @@
 #define ACCELERATION 2000  // steps per second per second
 
 /* HOLD TIMES */
-#define TOP_WAIT            3000    //millisceonds
-#define BOTTOM_WAIT         8000    //milliseconds
+#define TOP_WAIT            8000    //millisceonds
+#define BOTTOM_WAIT         3000    //milliseconds
 
 /* OTHER MAGIC NUMBERS */
 #define START_DELAY         1000
-#define ACTUATOR_CALIBRATE  98000  //Distance beyond actual actuator length for calibration purposes 
+#define ACTUATOR_CALIBRATE  26000   //Max height of travel
+#define START_CALIBRATE     100000  //Distance beyond actual actuator length for calibration purposes 
 #define INCH                1000    //Approx 3000 steps per inch
 #define INIT_DIRECTION      -1      //-1 is DOWN, 1 is UP
 #define UP_DIR              1
@@ -33,6 +34,8 @@ bool topTrigVal = true;
 AccelStepper stepper(1, PUL, DIR);  //AccelStepper constructor
 
 void setup() {
+  //Serial.begin(9600);
+
   /* pin declarations */
   pinMode(PUL, OUTPUT);
   pinMode(DIR, OUTPUT);
@@ -50,10 +53,11 @@ void setup() {
 
 /* run calibration sequence at power cycle */
 void initialRun(){
+  //Serial.print("Starting calibration\n");
   currDirection_ = INIT_DIRECTION;
   currDirection = INIT_DIRECTION;
   stepper.setCurrentPosition(0);        //Sets the current position as the 0 position arbitrarily. Will be reset during calibration sequence.
-  stepper.moveTo(INIT_DIRECTION*ACTUATOR_CALIBRATE);
+  stepper.moveTo(INIT_DIRECTION*START_CALIBRATE);
 }
 
 void checkSwitch(){
@@ -65,16 +69,24 @@ void loop() {
   checkSwitch();
   /* Actuator Calibration */
   if(bottomTrigVal==false && currDirection==DOWN_DIR){
+    // Serial.print("Bottom switch active, going up\n");
+    // Serial.print("bottomTrigVal ");
+    // Serial.print(bottomTrigVal);
+    // Serial.print("\n");
     currDirection_ = UP_DIR;
     stepper.setCurrentPosition(0);                //Sets the bottom switch as the bottom or 0 position         
     stepper.moveTo(ACTUATOR_CALIBRATE);
     delay(BOTTOM_WAIT);
     stepper.run();
   }
-  /* Actuator Calibration */
+  /* Fail safe */
   else if(topTrigVal==false && currDirection==UP_DIR){
+    // Serial.print("Top switch active, going down\n");
+    // Serial.print("topTrigVal ");
+    // Serial.print(topTrigVal);
+    // Serial.print("\n");
     currDirection_ = DOWN_DIR;
-    topmostPosition = stepper.currentPosition();  //Sets the top switch as the topmost position
+    topmostPosition = (stepper.currentPosition() - 3*INCH);  //Sets the topmost position as the top switch-3 inches
     stepper.moveTo(INCH);                         //One inch above 0 position
     delay(TOP_WAIT);
     stepper.run();
@@ -83,6 +95,7 @@ void loop() {
   else if(stepper.distanceToGo()==0){
     //if at top go down
     if(currDirection==UP_DIR){
+      //Serial.print("Reached top, going down\n");
       currDirection_ = DOWN_DIR;
       stepper.moveTo(INCH);
       delay(TOP_WAIT);
@@ -90,6 +103,7 @@ void loop() {
     }
     //if at bottom go up
     else if(currDirection==DOWN_DIR){
+      //Serial.print("Reached bottom, going up\n");
       currDirection_ = UP_DIR;
       stepper.moveTo(topmostPosition-INCH);   //One inch below topmost position
       delay(BOTTOM_WAIT);
